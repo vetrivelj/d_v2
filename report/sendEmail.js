63fdd53c0cf85d93e66959e8740eb2cdb5a026bf
GOKUL GOPALAKRISHNAN Nair <GOKULGOPALAKRN@hcl.com>
2019-02-28 12:04:42 +0530

    report

--- report/sendEmail.js
+++ report/sendEmail.js
@@ -16,11 +16,7 @@ module.exports = function(response, anaConfig, req, res, level, callback) {
     var suggests = [];
     var contextOut = [];
 
-
     try {
-
-
-
         console.log("Inside");
         // Create a SMTP transporter object
         var transporter = nodemailer.createTransport({
@@ -28,7 +24,7 @@ module.exports = function(response, anaConfig, req, res, level, callback) {
             auth: {
                 user: req.body.headers.emailuser,
                 pass: req.body.headers.emailpw
-            }
+            }	
         });
         var to_email = "";
         var reportName = "";
@@ -40,7 +36,11 @@ module.exports = function(response, anaConfig, req, res, level, callback) {
 
 
         if (intentName == 'ADS_HyperionReport') {
-            to_email = req.body.result.parameters.emailaddress;
+            if(req.body.result.parameters.emailaddress == "" || req.body.result.parameters.emailaddress == null)
+                    to_email = "gokulgnair94@gmail.com";
+            else
+                to_email = req.body.result.parameters.emailaddress;
+            
             reportName = req.body.result.parameters.reportName;
             yearName = req.body.result.parameters.reportYear;
 
@@ -52,14 +52,13 @@ module.exports = function(response, anaConfig, req, res, level, callback) {
             var projects = req.body.result.parameters["projects"];
 
             speech = 'Report : ' + reportName + ' for ' + projects + ' - ' + chartfield + ' (' + yearName + ') has been emailed to ' + to_email + '. Please give a few minutes for the email to arrive in your inbox. Is there anything else I can help you with?';
-            file = "DepartmentalExpenses_Corporate_Report.pdf";
+            file = "DepartmentalExpenses_Corporate_Report.txt";
             body = '<p><b>Hello,</b></p>' +
                 '<p>Attached is the Departmental Expenses Corporate Report as Requested.</p>' +
-                '<p>Thanks,<br><b>Viki</b></p>';
+                '<p>Thanks,<br><b>Aura</b></p>';
             subject = 'Departmental Expenses Corporate Report';
         } else {
             if (intentName == 'reporting') {
-                to_email = req.body.result.parameters.email || "gokulgnair94@gmail.com";
                 reportName = req.body.result.parameters.reportName;
                 yearName = req.body.result.parameters.year;
 
@@ -70,61 +69,62 @@ module.exports = function(response, anaConfig, req, res, level, callback) {
                 file = "PTVPLAN_PPCMRC_ReconReport.pdf";
                 body = '<p><b>Hello,</b></p>' +
                     '<p>Attached is the PTVPLAN and PPCMRC Reconcilition report as Requested.</p>' +
-                    '<p>Thanks,<br><b>Viki</b></p>';
-                subject = 'Reconcilition report: Budget - PTVPLAN and PPCMRC';
+                    '<p>Thanks,<br><b>Aura</b></p>';
+                subject = "Reconcilition report: " + scenario + " - PTVPLAN and PPCMRC";
             }
         }
-
-
-
 
         console.log(speech);
         console.log('SMTP Configured');
         fs.readFile("./report/" + file, function(err, data) {
             // Message object
-            var message = {
-                from: 'VIKI <' + req.body.headers.emailuser + '>',
-                // Comma separated list of recipients
-                to: to_email,
+            if(err){
+                console.log("Error :" + err);
+            }else{
+                    var message = {
+                    from: 'Aura <' + req.body.headers.emailuser + '>',
+                    // Comma separated list of recipients
+                    to: to_email,
 
-                bcc: 'gokulgnair94@gmail.com',
+                    bcc: 'gokulgnair94@gmail.com',
 
-                // Subject of the message
-                subject: subject, //
+                    // Subject of the message
+                    subject: subject, //
 
-                // HTML body
-                html: body,
+                    // HTML body
+                    html: body,
 
-                // Apple Watch specific HTML body
-                watchHtml: '<b>Hello</b> to myself',
+                    // Apple Watch specific HTML body
+                    watchHtml: '<b>Hello</b> to myself',
 
-                //An array of attachments
-                attachments: [{
-                    'filename': file,
-                    'content': data
-                }]
+                    //An array of attachments
+                    attachments: [{
+                        'filename': file,
+                        'content': data
+                    }]
 
-            };
+                };
 
-            transporter.verify(function(error, success) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Server is ready to take our messages');
-                }
-            });
+                transporter.verify(function(error, success) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Server is ready to take our messages');
+                    }
+                });
 
-            console.log('Sending Mail');
-            transporter.sendMail(message, (error, info) => {
-                if (error) {
-                    console.log('Error occurred');
-                    console.log(error.message);
-                    return;
-                }
-                console.log('Message sent successfully!');
-                console.log('Server responded with "%s"', info.response);
-                transporter.close();
-            })
+                console.log('Sending Mail');
+                transporter.sendMail(message, (error, info) => {
+                    if (error) {
+                        console.log('Error occurred');
+                        console.log(error.message);
+                        return;
+                    }
+                    console.log('Message sent successfully!');
+                    console.log('Server responded with "%s"', info.response);
+                    transporter.close();
+                })
+            }
         });
         return res.json({
             speech: speech,
